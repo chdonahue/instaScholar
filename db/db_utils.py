531b7cd131f308da818e502bc607c0758287e873
Christopher Donahue <christopherdonahue@ChristophersMBP.hsd1.ca.comcast.net>
2024-09-13 16:44:40 -0700

    Added function to allow field entry into the db

--- db/db_utils.py
+++ db/db_utils.py
@@ -1,18 +1,20 @@
 from google.cloud import firestore
-
+import logging
 
 def write_data(db, collection, document_id, data, override = False):
     """
     Adds new data to the firestore db
         Args: db (database obj): instascholar
               collection (str): 'papers'
-              document_id (str): ISSN (primary key)
+              document_id (str): doi (primary key)
               data (dict)
               override (bool): If set to True, will override an existing entry
     """
+    data.update({'timestamp': firestore.SERVER_TIMESTAMP})
+
     doc_ref = db.collection(collection).document(document_id)
     if doc_ref.get().exists and not override:
-        print('Entry already exists')
+        logging.info(f"{document_id} from collection {collection} already exists")
     else:
         doc_ref.set(data)
 
@@ -23,7 +25,7 @@ def read_data(db, collection, document_id):
         Args:
             db (databse obj): 'instascholar'
             collection (str): 'papers'
-            document_id (str): ISSN (primary key)
+            document_id (str): doi (primary key)
         Returns:
             Data dictionary (dict)
     """
@@ -33,3 +35,27 @@ def read_data(db, collection, document_id):
         return doc.to_dict()
     else:
         return None
+    
+
+def remove_entry(db, collection, document_id):
+    """
+    Removes entry from db
+        Args:
+            db (databse obj): 'instascholar'
+            collection (str): 'papers'
+            document_id (str): doi (primary key)
+        Returns:
+            Data dictionary (dict)
+    """
+    try:
+        # Create a reference to the document
+        doc_ref = db.collection(collection).document(document_id)
+        
+        # Delete the document
+        doc_ref.delete()
+        
+        logging.info(f"Successfully deleted document with ID: {document_id} from collection: {collection}")
+        return True
+    except Exception as e:
+        logging.info(f"An error occurred while deleting the document: {e}")
+        return False
